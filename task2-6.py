b8b5ac4ead406a50b65dde68a3bb650aba7d9cfd
Svetlana233 <foxy_sv@mail.ru>
2020-04-15 00:13:59 +0300

    add tasks2-6

--- task2-6.py
+++ task2-6.py
@@ -9,31 +9,41 @@ while True:
 good_list = []
 i = 0
 while i < goods:
-    name = input('название продукта ')
-    price = input('цена продукта ')
-    price = int(price)
-    num = input('количество продукта ')
-    num = int(num)
+    name = input('Name of the product ')
+    while True:
+        price = input('Product`s price ')
+        if price.isdigit():
+            price = int(price)
+            break
+        else:
+            print('You enter the wrong number. Try again')
+    while True:
+        num = input('Product`s quantity ')
+        if q.isdigit():
+            q = int(q)
+            break
+        else:
+            print('You enter the wrong number. Try again')
     meas = input('ед измерения продукта ')
-    good_list.append((i + 1, {'name': name, 'price': price, 'number': num, 'measure': meas}))
+    good_list.append((i + 1, {'name': name, 'price': price, 'quantity': q, 'measure': meas}))
     i += 1
-# good_dict = {}
+
 name_list = []
 price_list = []
-number_list = []
+quantity_list = []
 measure_list = []
 
 for good in good_list:
     i = good[1]
     name_list.append(i["name"])
     price_list.append(i["price"])
-    number_list.append(i["number"])
+    quantity_list.append(i["quantity"])
     measure_list.append(i["measure"])
 
 print(good_list)
 print({
 'название': name_list,
 'цена': price_list,
-'количество': number_list,
+'количество': quantity_list,
 'ед': measure_list
 })
