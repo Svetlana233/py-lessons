61740c4c5e026033f986b46d92c7fe227653e3d5
Svetlana233 <foxy_sv@mail.ru>
2020-04-22 16:33:25 +0300

    update tasks4-2 add task4-4

--- task4-4.py
+++ task4-4.py
@@ -1,12 +1,11 @@
 from random import randint
 ls = []
-ls = [randint(1, 10) for el in range(1, 15)]
+ls = [randint(0, 10) for el in range(1, 15)]
 print(ls)
-
 new_list = []
 
 for j in range(len(ls)):
-    if (j > 0) and (ls[j] > ls[j-1]):
+    if ls.count(ls[j]) == 1:
         new_list.append(ls[j])
 
 print(new_list)
