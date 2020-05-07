344b49602fdaed31d720d75b536c60d4a8ec19e7
Svetlana233 <foxy_sv@mail.ru>
2020-05-07 22:04:30 +0300

    update task8-5

--- task8-5.py
+++ task8-5.py
@@ -1,26 +1,26 @@
 class Warehouse():
     name = ''
     my_dict = {'printer': 0, 'scanner': 0, 'copier': 0}
+    sub_dict = {'printer': 0, 'scanner': 0, 'copier': 0}
 
     def __init__(self, indoor, outdoor):
         self.indoor = indoor
         self.outdoor = outdoor
 
-
+# пополнение склада:
     def get_in(self):
         self.my_dict[self.name] = self.indoor
         return self.my_dict.items()
 
-    def get_out(self, y):
-        self.y = y
-
-
+# передача техники в департамент:
+    def get_out(self):
+        self.sub_dict[self.name] = self.outdoor
+        return self.sub_dict.items()
 
 class Equipment():
     def __init__(self, price, quantity):
         self.price = price
         self.quantity = quantity
-
     def total_price(self):
         return self.price * self.quantity
 
@@ -45,15 +45,20 @@ class Copier(Equipment, Warehouse):
         Warehouse.__init__(self, indoor, outdoor)
         self.performance = performance
 
-p = Printer(1000, 20, 15, 20, "white")
-print(p.color)
-print(p.total_price())
-print(p.get_in())
-
-s = Scanner(3000, 5, 40)
-print(s.weight)
-print(s.total_price())
-c = Copier(1500, 10, 400)
-print(c.performance)
-print(c.total_price())
-
+p = Printer(1000, 20, 15, 10, "white")
+print('The color of printers is', p.color)
+print('Total price for all printers', p.total_price(), 'Rub')
+print('Current stock status: ', p.get_in())
+print('At the disposal of the finance department:', p.get_out())
+print('________________________________________________________________________________')
+s = Scanner(3000, 5, 3, 1, 40)
+print('weight of scanner:', s.weight, 'kg')
+print('Total price for all scanners', s.total_price(), 'Rub')
+print('Current stock status: ', s.get_in())
+print('At the disposal of the finance department:', s.get_out())
+print('________________________________________________________________________________')
+c = Copier(1500, 10, 5, 4, 400)
+print('Performance of the copier is ', c.performance, 'sheets per minute')
+print('Total price for all copiers', c.total_price(), 'Rub')
+print('Current stock status: ', c.get_in())
+print('At the disposal of the finance department:', c.get_out())
